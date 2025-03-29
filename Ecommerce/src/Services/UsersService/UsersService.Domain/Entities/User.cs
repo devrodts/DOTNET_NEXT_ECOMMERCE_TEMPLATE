b371ba424070f5b0539f86acea8c1ce594129c15
rod <dev_rodts@outlook.com>
2025-03-29 01:29:00 -0300

    feat: add skeleton for user entity

---
+++ Ecommerce/src/Services/UsersService/UsersService.Domain/Entities/User.cs
@@ -0,0 +1,32 @@
+﻿namespace Ecommerce.src.Services.UsersService.UsersService.Domain.Entities
+{
+    public class User
+    {
+        public Guid UserId { get; private set; }
+        public string Email { get; private set; }
+        public string Name { get; private set; }
+        public string PasswordHash { get; private set; }
+
+        protected User()
+        {
+            Email = string.Empty;
+            Name = string.Empty;
+            PasswordHash = string.Empty;
+        }
+
+        public User(string email, string passwordHash)
+        {
+            UserId = Guid.NewGuid();
+            Email = email;
+            PasswordHash = passwordHash;
+            Name = string.Empty;
+        }
+
+        public void UpdateName(string name)
+        {
+            Name = name;
+        }
+
+
+    }
+}
