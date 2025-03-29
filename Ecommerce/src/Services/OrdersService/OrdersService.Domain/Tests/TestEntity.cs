46d6b073b268e5ee07533d3ee520451590a91bb3
rod <dev_rodts@outlook.com>
2025-03-29 13:20:49 -0300

    test: add BuyerRepositoryTest

---
+++ Ecommerce/src/Services/OrdersService/OrdersService.Domain/Tests/TestEntity.cs
@@ -0,0 +1,35 @@
+﻿using Ecommerce.src.Services.OrdersService.OrdersService.Domain.SeedWork;
+using MediatR;
+using Xunit;
+
+namespace Ecommerce.src.Services.OrdersService.Tests
+{
+    public class TestEntityTests
+    {
+        [Fact]
+        public void Constructor_SetsId()
+        {
+            var newId = Guid.NewGuid();
+            var entity = new TestEntity(newId);
+            Assert.Equal(newId, entity.Id);
+        }
+
+        [Fact]
+        public void AddDomainEvent_AddsEventToDomainEvents()
+        {
+            var entity = new TestEntity(Guid.NewGuid());
+            var domainEvent = new DomainEventTest();
+            entity.AddDomainEvent(domainEvent);
+            Assert.Contains(domainEvent, entity.DomainEvents);
+        }
+    }
+
+    public class TestEntity : Entity
+    {
+        public TestEntity(Guid id)
+        {
+            Id = id;
+        }
+    }
+    public class DomainEventTest : INotification { }
+}
